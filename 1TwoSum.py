a19ae66d2cd1d3712695d3ffbdab73b891c5fc93
orangemarmalade88 <orangemarmalade88@gmail.com>
2025-04-11 18:34:17 +0800

    Update 1TwoSum.py

--- 1TwoSum.py
+++ 1TwoSum.py
@@ -1,5 +1,11 @@
 class Solution(object):
     def twoSum(self, nums, target):
         dict = {}
-        for i in nums:
-            if dict.
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in dict:
+                return [dict[comp], i]
+            else:
+                dict[nums[i]] = i
+        return [-1, -1]
+    
